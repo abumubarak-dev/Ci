b2e93227e2566c8e8dddc56e5c9c55f423a0eb0f
ismail <ismaildeve070@gmail.com>
2021-01-20 02:05:14 +0100

    Checking pull request2

--- app/src/main/java/com/example/ci/MainActivity.java
+++ app/src/main/java/com/example/ci/MainActivity.java
@@ -10,6 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //commit for test branch
-        setContentView(R.layout.activity_main);
-    }
+     }
 }
